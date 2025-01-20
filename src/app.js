aed6c34575673c3818a8c9cc12b263d7b66ca103
ww-ahyuda <wahyuda542@smk.belajar.id>
2025-01-20 13:17:06 +0700

    Perbaikan Server,Implementasi CRUD Finance(Buat Controller untuk Finance)

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const dotenv = require('dotenv'); 
+const connectDB = require('./config/db'); 
 
 dotenv.config();
-
 const app = express();
-
 app.use(express.json());
+const userRoutes = require('./routes/userRoutes');
+// const financeRoutes = require('./routes/financeRoutes');
 
+app.use('/api/users', userRoutes);
+// app.use('/api/finance', financeRoutes);
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server berjalan di port ${PORT}`));
 
 connectDB();
