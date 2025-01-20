7e8cd7311737f34b4b85be24e65086b9614de948
ww-ahyuda <wahyuda542@smk.belajar.id>
2025-01-20 15:08:46 +0700

    Perbaikan Server,Implementasi CRUD Finance(Buat Route Untuk Finance dan Integrasikan Route ke Server)

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 const userRoutes = require('./routes/userRoutes');
-// const financeRoutes = require('./routes/financeRoutes');
+const financeRoutes = require('./routes/financeRoutes');
 
 app.use('/api/users', userRoutes);
-// app.use('/api/finance', financeRoutes);
+app.use('/api/finances', financeRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server berjalan di port ${PORT}`));
 
