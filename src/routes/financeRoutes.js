4d2c007f771853f9e75425d17976648ca271a37a
ww-ahyuda <wahyuda542@smk.belajar.id>
2025-02-04 11:36:02 +0700

    Perbarui Rute di financeRoutes.js

--- src/routes/financeRoutes.js
+++ src/routes/financeRoutes.js
@@ -10,11 +10,12 @@ const {
   getCategoryStats,
 } = require('../controllers/financeController');
 const { protect } = require('../middleware/authMiddleware');
+const { getMonthlyStats } = require('../controllers/financeController');
 
 router.route('/').get(protect, getFinances).post(protect, createFinance);
 router.route('/:id').put(protect, updateFinance).delete(protect, deleteFinance);
 router.get('/summary', protect, getFinanceSummary);
 router.get('/filter', protect, filterFinance);
 router.get('/category-stats', protect, getCategoryStats);
-
+router.get('/monthly-stats', protect, getMonthlyStats);
 module.exports = router;
